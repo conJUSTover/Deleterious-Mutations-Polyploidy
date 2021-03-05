7325934565782a7c0b8b5077b6bdd7f89922de29
jconover <jconover@phantoon.las.iastate.edu>
2021-03-05 13:19:14 -0600

    added slurm files and R file to concat results

---
+++ analysis/BAD_Mutations/Merge_subs_Combined_Results.R
@@ -0,0 +1,18 @@
+setwd("/work/LAS/jfw-lab/jconover/deleterious_mutations/BAD_runs/redo_Australian")
+
+BAD_table <- read.table("Combined_Report.txt", header = TRUE)
+
+subs <- read.table("all.GT.longform.subs")
+names(subs) <- c("gene", "aapos", "altaa", "SNP Name")
+
+##Create new variable for gene_aaPOS
+BAD_table$GeneAA <- apply(BAD_table[,c("GeneID", "CDSPos")], 1, paste, collapse = "_")
+subs$GeneAA <- apply(subs[,c("gene", "aapos")], 1, paste, collapse = "_")
+
+##Merge SNP reference and BAD_table, keeping all sub entries and duplicating 
+##SNPs occuring in same codon 
+new_BAD <- merge(BAD_table, subs, by.x = "GeneAA", by.y = "GeneAA", all.y = TRUE)
+
+#write to new file
+write.table(new_BAD, file = "all.GT.merged_results.txt", quote = FALSE, sep = "\t", row.names = FALSE)
+
